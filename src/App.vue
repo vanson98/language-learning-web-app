9604f373020749c8c1541856e83c5ce06e4997f6
vanson-ubuntu <vanson@gmail.com>
2023-09-06 17:53:06 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <LanguageLessons></LanguageLessons>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
-
+import LanguageLessons from "./components/LanguageLessons.vue";
 export default defineComponent({
   name: "App",
   components: {
-    HelloWorld,
+    LanguageLessons,
   },
 });
 </script>
---
+++ src/components/LanguageLessons.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>Xin chao Son den voi view</div>
+  <button @click="count++">You clicked me {{ count }} times :#.</button>
+  <el-input
+    v-model="lessonContent"
+    :autosize="{ minRows: 2, maxRows: 4 }"
+    type="textarea"
+    placeholder="Input your content here"
+  ></el-input>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import { ElInput } from "element-plus";
+import { axios} from "axios";
+const lessonContent = ref("");
+
+const count = ref(0);
+</script>
