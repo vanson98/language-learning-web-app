b27da34f8ff94ae23a447379611b6f9e88612378
vanson-ubuntu <vanson@gmail.com>
2023-09-15 17:42:50 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,9 @@
 /* eslint-disable */
 import { createApp } from "vue";
 import App from "./App.vue";
-import Home from "@/components/Home.vue";
-import LanguageLessons from "./components/LanguageLessons.vue";
 import "element-plus/dist/index.css";
-import { createRouter, createWebHistory } from "vue-router";
+import router from "./router";
 
-
-const router = createRouter({
-    history: createWebHistory(),
-    routes: [
-        { path: "/", name: "Home", component: Home },
-        { path: "/import-lesson", name: "Lesson", component: LanguageLessons },
-    ],
-});
 
 const app = createApp(App);
 app.use(router);
